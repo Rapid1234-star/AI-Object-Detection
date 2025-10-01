30d1b9227ec42b53bb84821dd711d63552790c06
Aayan Desai <aayanirshad@gmail.com>
2025-10-01 13:46:26 +0400

    first commit

---
+++ components/object-detection.js
@@ -0,0 +1,11 @@
+function ObjectDetection() {
+    return(
+    <div className="mt-8 bg-white">
+        <div>
+            {/* Your object detection component code here */}
+            {/*Canvas */}
+        </div>
+    </div>
+)
+}
+export default ObjectDetection;
